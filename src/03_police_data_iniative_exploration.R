19694309af9b1294e343019abf8adf906307d213
ckelling <ckelling@vt.edu>
2019-01-14 14:37:39 -0500

    exploration of police data initiative data for Cincinnati and Baltimore

---
+++ src/03_police_data_iniative_exploration.R
@@ -0,0 +1,64 @@
+###
+### Police Data Initiative
+### Exploration of city alternatives
+### 
+### Claire Kelling
+###
+### Last Updated: 1/14/19
+###
+
+library(ggplot2)
+
+# Load data for other cities
+#    Source: https://www.policedatainitiative.org/datasets/calls-for-service/
+balt_dat <- read.csv(file = "C:/Users/ckell/Desktop/Google Drive/Box Sync/claire_murali_sesa_group/crime/crime_spatial_displacement/data/original/Baltimore_911_Police_Calls_for_Service.csv")
+cinci_dat <- read.csv(file = "C:/Users/ckell/Desktop/Google Drive/Box Sync/claire_murali_sesa_group/crime/crime_spatial_displacement/data/original/Cincinnati_PDI__Police_Data_Initiative__Police_Calls_for_Service__CAD_.csv")
+
+# Number of calls for service:
+nrow(balt_dat) #about 4 million calls for service
+nrow(cinci_dat) #about 2.5 million calls for service
+
+# Convert data/time to see how many years of data we have
+balt_dat$callDateTime <- as.character(balt_dat$callDateTime)
+balt_dat$callDateTime[1]
+balt_dat$datetime <- as.POSIXct(balt_dat$callDateTime,format="%m/%d/%Y %H:%M:%S %p",tz=Sys.timezone())
+range(balt_dat$datetime, na.rm = T)
+#"2015-01-01 01:00:00 EST" "2019-01-14 12:59:00 EST"
+
+cinci_dat$DISPATCH_TIME_PRIMARY_UNIT <- as.character(cinci_dat$DISPATCH_TIME_PRIMARY_UNIT)
+cinci_dat$DISPATCH_TIME_PRIMARY_UNIT[1]
+cinci_dat$datetime <- as.POSIXct(cinci_dat$DISPATCH_TIME_PRIMARY_UNIT,format="%m/%d/%Y %H:%M:%S %p",tz=Sys.timezone())
+range(cinci_dat$datetime, na.rm = T)
+#"2014-10-01 01:00:18 EDT" "2019-01-14 12:58:59 EST"
+
+
+# Plot over space
+# Load maps for plotting, created in 00_get_maps.R
+load(file = "C:/Users/ckell/Desktop/Google Drive/Box Sync/claire_murali_sesa_group/crime/crime_spatial_displacement/data/working/Baltimore_Map.Rdata")
+load(file = "C:/Users/ckell/Desktop/Google Drive/Box Sync/claire_murali_sesa_group/crime/crime_spatial_displacement/data/working/Cincinnati_Map.Rdata")
+
+#need to get lat and long for Baltimore
+balt_dat$location <- as.character(balt_dat$location)
+balt_dat$coordinates <- gsub("[\\(\\)]", "", regmatches(balt_dat$location, gregexpr("\\(.*?\\)", balt_dat$location))[[1]])
+
+#after comma
+balt_dat$Longitude <- as.numeric(sub('.*,\\s*','', balt_dat$coordinates))
+
+#before comma
+balt_dat$Latitude <- as.numeric(sub('\\s*,.*','', balt_dat$coordinates))
+
+#Subset to high priority calls
+balt_dat$priority <- as.character(balt_dat$priority)
+balt_high <- balt_dat[which(balt_dat$priority == "High"),]
+cinci_high <- cinci_dat[which(cinci_dat$PRIORITY < 5),]
+
+BaltimoreMap  + geom_point(aes(x = Longitude, y = Latitude), size = 1,
+                         data = balt_high, col = "blue", alpha =0.01) + coord_equal() +
+  ggtitle("Point Data, Baltimore High Priority Calls")+
+  theme(text = element_text(size=30))+theme(axis.text.x=element_text(size=20))
+
+# Plot for Cincinnati Data
+CincinnatiMap  + geom_point(aes(x = LONGITUDE_X, y = LATITUDE_X), size = 1,
+                           data = cinci_high, col = "blue", alpha =0.01) + coord_equal() +
+  ggtitle("Point Data, Cincinnati High Priority Calls")+
+  theme(text = element_text(size=30))+theme(axis.text.x=element_text(size=20))
